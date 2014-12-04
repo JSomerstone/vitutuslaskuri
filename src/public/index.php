1c63ee8d2b2d70b9452a9a53e07e06c7f0d62d5d
Joona Somerkivi <joona.somerkivi@solinor.com>
2014-12-04 14:12:22 +0200

    Showing pissed-off-meter

--- src/public/index.php
+++ src/public/index.php
@@ -8,31 +8,32 @@ $response = array(
     'message' => array(),
     'notice' => array(),
     'error' => array(),
-    'statistics' => array($today => array('count' => 0, 'average' => 0, 'sum' => 0))
+    'statistics' => array('count' => 0, 'average' => 0, 'sum' => 0, 'width' => 0)
 );
 
 if (isset($_POST['pissedOff']))
 {
     if (isset($_POST['nope'])) $pissedOffValue = 0;
     elseif (isset($_POST['bit'])) $pissedOffValue = 1;
-    elseif (isset($_POST['more'])) $pissedOffValue = 3;
-    elseif (isset($_POST['lot'])) $pissedOffValue = 5;
+    elseif (isset($_POST['more'])) $pissedOffValue = 2;
+    elseif (isset($_POST['lot'])) $pissedOffValue = 4;
 
     $statisticsFile = '/tmp/vitutuslaskuri.json';
     $today = date('Y-m-d');
     $statistics = file_exists($statisticsFile)
         ? json_decode(file_get_contents($statisticsFile), true)
-        : array($today => array('count' => 0, 'average' => 0, 'sum' => 0));
+        : array($today => array('count' => 0, 'average' => 0, 'sum' => 0, 'width' => 0));
 
     $statistics[$today] = array(
         'count' => $count = $statistics[$today]['count'] + 1,
         'sum' => $sum = $statistics[$today]['sum'] + $pissedOffValue,
-        'average' => $average = $sum / $count
+        'average' => $average = $sum / $count,
+        'width' => (int)floor($average * 3)
     );
 
     file_put_contents($statisticsFile, json_encode($statistics));
     $response['message'][] = 'Vitutuksesi aste huomioitu';
-    $response['statistics'] = $statistics;
+    $response['statistics'] = $statistics[$today];
 }
 
 echo $twig->render('page.html.twig', $response);
