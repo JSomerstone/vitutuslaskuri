e1a40c5cb1438f2518f703836bdf2bdc0ff8f4fc
Joona Somerkivi <joona.somerkivi@solinor.com>
2014-12-04 14:01:13 +0200

    Storing statistics

--- src/public/index.php
+++ src/public/index.php
@@ -3,5 +3,37 @@ include_once __DIR__ . '/../../vendor/Twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem(realpath(__DIR__ . '/../views/'));
 $twig = new Twig_Environment($loader);
-echo $twig->render('page.html.twig', array('text' => 'Hello world!'));
+
+$response = array(
+    'message' => null,
+    'notice' => null,
+    'error' => null,
+    'statistics' => array()
+);
+
+if (isset($_POST['pissedOff']))
+{
+    if (isset($_POST['nope'])) $pissedOffValue = 0;
+    elseif (isset($_POST['bit'])) $pissedOffValue = 1;
+    elseif (isset($_POST['more'])) $pissedOffValue = 3;
+    elseif (isset($_POST['lot'])) $pissedOffValue = 5;
+
+    $statisticsFile = '/tmp/vitutuslaskuri.json';
+    $today = date('Y-m-d');
+    $statistics = file_exists($statisticsFile)
+        ? json_decode(file_get_contents($statisticsFile), true)
+        : array($today => array('count' => 0, 'average' => 0, 'sum' => 0));
+
+    $statistics[$today] = array(
+        'count' => $count = $statistics[$today]['count'] + 1,
+        'sum' => $sum = $statistics[$today]['sum'] + $pissedOffValue,
+        'average' => $average = $sum / $count
+    );
+
+    file_put_contents($statisticsFile, json_encode($statistics));
+    $response['message'] = 'Vitutuksesi aste huomioitu';
+    $response['statistics'] = $statistics;
+}
+
+echo $twig->render('page.html.twig', $response);
 
